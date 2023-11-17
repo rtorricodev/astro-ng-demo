37174796c9632b8d9b4f638c425fa297d14ceb78
rtorricodev <rodrigo.torricodelcastillo@gmail.com>
2023-11-17 16:04:12 -0400

    🚧 initial commit

---
+++ src/components/hello.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-hello',
+  standalone: true,
+  imports: [NgIf],
+  template: `
+    <p>Hello from Angular!!</p>
+
+    <p *ngIf="show">{{ helpText }}</p>
+
+    <button (click)="toggle()">Toggle</button>
+  `,
+})
+export class HelloComponent {
+  @Input() helpText = 'help';
+
+  show = false;
+
+  toggle() {
+    this.show = !this.show;
+
+  }
+}
